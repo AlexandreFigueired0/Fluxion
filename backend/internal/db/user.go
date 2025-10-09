954e8927d563936132ba7f73963c69ba19579590
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2025-10-09 19:25:42 +0000

    Started user db service

--- backend/internal/db/user.go
+++ backend/internal/db/user.go
@@ -1,1 +1,22 @@
 package db
+
+import (
+	supa "github.com/supabase-community/supabase-go"
+)
+
+type User struct {
+	ID               string
+	Email            string
+	Password         string
+	CreatedAt        string
+	UpdatedAt        string
+	Credits          int
+	StripeCustomerID string
+	Name             string
+}
+
+func GetUserByApiKey(conn *supa.Client, apiKey string) (*User, error) {
+	var user User
+
+	return &user, nil
+}
--- backend/main.go
+++ backend/main.go
@@ -12,12 +12,7 @@ import (
 const address = "localhost:8080"
 
 func main() {
-	// Setup logger
-	logFile, err := os.OpenFile("server.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("Failed to open log file: %v", err)
-	}
-	log.SetOutput(logFile)
+	log.SetOutput(os.Stdout)
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	r := gin.Default()
