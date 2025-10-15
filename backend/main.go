70eb624c85f98e00de223a354be34996feab186e
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2025-10-15 18:20:47 +0000

    Frontend can now fetch user info

--- backend/main.go
+++ backend/main.go
@@ -5,7 +5,9 @@ import (
 	"fluxion-be/internal/handlers"
 	"log"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,6 +18,15 @@ func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"}, // your Next.js dev URL
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	db_conn, err := db.NewClient()
 	if err != nil {
