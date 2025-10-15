a2ce92771fbb05647d9bc982a31c0ab6f9f74cc1
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2025-10-15 16:24:27 +0000

    Created UserDTO

--- backend/internal/handlers/users.go
+++ backend/internal/handlers/users.go
@@ -12,6 +12,14 @@ type UserHandler struct {
 	DB *supa.Client
 }
 
+// Create user type with only the fields we want to expose
+type UserDTO struct {
+	ID      string `json:"id"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Credits int    `json:"credits"`
+}
+
 // GetUserByID retrieves a user by their ID.
 func (h *UserHandler) GetUserByID(c *gin.Context) {
 	id := c.Param("id")
@@ -20,5 +28,11 @@ func (h *UserHandler) GetUserByID(c *gin.Context) {
 		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
 		return
 	}
-	c.JSON(http.StatusOK, user)
+	userResponse := UserDTO{
+		ID:      user.ID,
+		Name:    user.Name,
+		Email:   user.Email,
+		Credits: user.Credits,
+	}
+	c.JSON(http.StatusOK, userResponse)
 }
