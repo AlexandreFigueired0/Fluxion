c94740915b25e76ab314ee02d698ff8c8cdf1068
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2025-10-19 11:24:12 +0000

    Created pipeline dto

---
+++ backend/internal/dto/pipeline.go
@@ -0,0 +1,14 @@
+package dto
+
+import "time"
+
+// PipelineDTO represents the data transfer object for a pipeline.
+type PipelineDTO struct {
+	ID          string    `json:"id"`
+	UserID      string    `json:"user_id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	ConfigYAML  string    `json:"config_yaml"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
