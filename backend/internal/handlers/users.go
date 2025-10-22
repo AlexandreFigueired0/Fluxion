da4874b57b68c4b47081be5c50a42a188ba4da99
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2025-10-22 11:42:26 +0000

    Added auth in GetUserById endpoint

--- backend/internal/handlers/users.go
+++ backend/internal/handlers/users.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	supa "github.com/supabase-community/supabase-go"
 )
 
@@ -16,6 +17,19 @@ type UserHandler struct {
 // GetUserByID retrieves a user by their ID.
 func (h *UserHandler) GetUserByID(c *gin.Context) {
 	id := c.Param("id")
+
+	// Auth user
+	claims, exists := c.Get("user")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+	userClaims := claims.(jwt.MapClaims)
+	if userClaims["id"] != id {
+		c.JSON(http.StatusForbidden, gin.H{"error": "Cannot access other user's data"})
+		return
+	}
+
 	user, err := db.GetUserByID(id, h.DB)
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
