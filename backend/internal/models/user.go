d49c1559a5705f3bb381f262c02eff50e0fb0e90
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2025-10-26 14:34:39 +0000

    Updated user db model

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -3,14 +3,17 @@ package models
 import "time"
 
 type User struct {
-	ID               string    `json:"id"`
-	Email            string    `json:"email"`
-	CreatedAt        time.Time `json:"created_at"`
-	PasswordHash     string    `json:"password_hash"`
-	Credits          int       `json:"credits"`
-	StripeCustomerID string    `json:"stripe_customer_id"`
-	Name             string    `json:"name"`
-	UpdatedAt        time.Time `json:"updated_at"`
-	Provider         string    `json:"provider"`    // "credentials", "google", "github"
-	ProviderID       string    `json:"provider_id"` // e.g. Google or GitHub user ID
+	ID                    string    `json:"id"`
+	Email                 string    `json:"email"`
+	CreatedAt             time.Time `json:"created_at"`
+	PasswordHash          string    `json:"password_hash"`
+	PermanentCredits      int       `json:"permanent_credits"`
+	SubscritpionCredits   int       `json:"subscription_credits"`
+	StripeCustomerID      string    `json:"stripe_customer_id"`
+	SubscriptionPlanID    string    `json:"subscription_plan_id"`
+	SubscriptionPeriodEnd time.Time `json:"subscription_period_end"`
+	Name                  string    `json:"name"`
+	UpdatedAt             time.Time `json:"updated_at"`
+	Provider              string    `json:"provider"`    // "credentials", "google", "github"
+	ProviderID            string    `json:"provider_id"` // e.g. Google or GitHub user ID
 }
